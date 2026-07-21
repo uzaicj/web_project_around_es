2545232e9498dac061c56e62d7b15e165a47ca35
uzaí <uzai.cj@gmail.com>
2026-07-20 22:58:27 -0600

    Deja de trackear JS generados en public/types y public/utils

--- public/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const defaultFormConfig = {
-    popupInput: '.popup__input',
-    popupButton: '.popup__button',
-    popupButtonDisabled: 'popup__button_disabled',
-    popupInputTypeError: 'popup__input_type_error',
-    popupInputErrorActive: 'popup__input-error_active',
-};
